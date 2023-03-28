3c7384f0348af774488b43d90b753056a1289e5c
Aaron Regalado <regalado2829@gmail.com>
2023-03-27 21:34:04 -0400

    Adding Logo and Working on Nav Bar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,26 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Fragment } from 'react';
+import { Outlet, Link } from "react-router-dom";
+
+import RegapiLogo from "../../assets/regapi-logo.png";
+
+import "./navigation.styles.scss";
 
 
 function Nav(){
     return (
-      <div> 
-        <div>
-          <h1>I am the Nav Bar</h1>
+      <Fragment> 
+        <div className="navigation">
+            <Link className="logo-container" to="/">
+                <img src={RegapiLogo} className="logo" />
+            </Link>
+            <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">
+                SHOP
+            </Link>
+          </div>
         </div>
         <Outlet />
-      </div>
+      </Fragment>
     )
   }
 
