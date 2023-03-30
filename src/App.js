00b00e3cbb3581e34198cd9db9ee941acdfb341b
Aaron Regalado <regalado2829@gmail.com>
2023-03-29 23:11:28 -0400

    Starting to Add Firebase to Project

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 
 import Nav from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 function App() {
+
+  const Shop = () => {
+    return (
+      <h1>I am the shop page</h1>
+    )
+  }
 
   return (
     <Routes>
       <Route path="/" element={<Nav />}>
         <Route index element={<Home />}/>
+        <Route path="shop" element={<Shop />}/>
+        <Route path="sign-in" element={<SignIn />}/>
       </Route>
     </Routes>
   )
